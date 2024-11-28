476d2cc66cce4e385d3adf53bb434446cacfe6be
jyb-maker <bjy7328@naver.com>
2024-11-28 11:11:09 +0900

    회원가입 유효성 검사 추가

--- src/main/webapp/joinCheck.js
+++ src/main/webapp/joinCheck.js
@@ -11,18 +11,47 @@ function joinCheck() {  // 유효성 체크 함수 joinCheck 선언
 		alert("아이디는 필수 입력 사항입니다.\n다시 확인후 가입해 주세요.");
 		joinForm.memberId.focus(); //해당 폼 위치로 커서를 이동 시킴 
 		return false;		
- } if(joinForm.memberPw.value.length == 0) {
+ } 
+ /* 아이디가 4자 이상 15자 이하만 가능하다록 유효성 검사 */
+  if(joinForm.memberId.value.length <4 || joinForm.memberId.value.length >15) {
+ 	alert("아이디는 4자 이상 15자 이하만 가능합니다.\n다시 확인후 가입해 주세요.");
+ 	joinForm.memberId.focus(); //해당 폼 위치로 커서를 이동 시킴 
+ 	return false;		
+ } 
+ /* 비번이 4자 이상 10자 이하만 가능하다록 유효성 검사 */
+ 	if(joinForm.memberPw.value.length == 0) {
 	  alert("비번은 필수 입력 사항입니다.\n다시 확인후 가입해 주세요.");
 	  joinForm.memberPw.focus(); //해당 폼 위치로 커서를 이동 시킴 
-	  return false; }					
+	  return false; }
+if(joinForm.memberPw.value.length <4 || joinForm.memberPw.value.length >10) {
+	  alert("비번은 4자 이상 10자 이하만 가능합니다.\n다시 확인후 가입해 주세요.");
+	  joinForm.memberPw.focus(); //해당 폼 위치로 커서를 이동 시킴 
+	  return false;		}	  
+	  	  	  					
    if(joinForm.memberName.value.length == 0) {
 	  alert("이름은 필수 입력 사항입니다.\n다시 확인후 가입해 주세요.");
 	  joinForm.memberName.focus(); //해당 폼 위치로 커서를 이동 시킴 
 	  return false; }		
+	/* 이름은 한글만 입력되도록 유효성 검사 */   
+	var hangul = /^[ㄱ-ㅎ|가-힣]*$/;  // 한글만 가능   
+	var eng = /^[a-z|A-Z]*$/; // 영어만 가능  
+	var number = /^[0-9]*$/; // 영어만 가능  
+
+	if(!hangul.test(joinForm.memberName.value)) {
+	 	 alert("이름은 한글만 입력 가능합니다.\n다시 확인후 가입해 주세요.");
+	  	 joinForm.memberName.focus(); //해당 폼 위치로 커서를 이동 시킴 
+	  	 return false; }				  
+	
+	
    if(joinForm.memberPhone.value.length == 0) {
 	  alert("전화번호는 필수 입력 사항입니다.\n다시 확인후 가입해 주세요.");
 	  joinForm.memberPhone.focus(); //해당 폼 위치로 커서를 이동 시킴 
 	  return false; 
   }	
+  /* 전화번호는 숫자만 입력 가능하도록 유효성 검사*/
+  if(!number.test(joinForm.memberPhone.value)) {
+   	 alert("이름은 한글만 입력 가능합니다.\n다시 확인후 가입해 주세요.");
+    	 joinForm.memberPhone.focus(); //해당 폼 위치로 커서를 이동 시킴 
+    	 return false; }	
  	return true;
 }
